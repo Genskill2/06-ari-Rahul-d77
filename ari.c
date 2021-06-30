e5b4630371d7f070b86ec455b7a9a61f8daf0843
Rahul-d77 <84717074+Rahul-d77@users.noreply.github.com>
2021-06-30 14:29:32 +0530

    ari.c

--- ari.c
+++ ari.c
@@ -31,33 +31,18 @@ string ari(string s)
   switch (c) 
   {
     case 1: return "Kindergarten"; 
-       //     break;
     case 2: return "First/Second grade";
-          //  break;
     case 3: return "Third Grade";
-         //   break;
     case 4: return "Fourth Grade";
-         //   break;
     case 5: return "Fifth Grade";
-        //    break;
     case 6: return "Sixth Grade";
-        //    break;
     case 7: return "Seventh Grade";
-       //   break;
     case 8: return "Eighth Grade";
-         //   break;
     case 9: return "Ninth Grade";
-        //    break;
     case 10: return "Tenth Grade";
-        //     break;
     case 11: return "Eleventh Grade";
-         //    break;
     case 12: return "Twelfth Grade";
-           //  break;
     case 13: return "College student";
-         //    break;
     case 14: return "Professor";
-           //  break;
-    
    }
 }
